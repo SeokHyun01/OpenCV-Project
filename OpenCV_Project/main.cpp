488ae34767cee78c82e44072684e0460cf641bb1
SeokHyun Youn <cheongyeon2018@gmail.com>
2022-09-02 18:02:37 +0900

    주석 추가

--- OpenCV_Project/main.cpp
+++ OpenCV_Project/main.cpp
@@ -57,13 +57,18 @@ int main(void)
 		absdiff(previous, current, diff);
 		imshow("diff", diff);
 
+		// 모폴로지 팽창 연산
+		// 일부 노이즈 제거
 		dilate(diff, dilation, Mat::ones(Size(5, 5), CV_8UC1));
 
+		// OTSU method를 통해 이진화
 		threshold(dilation, bin, 0, 255, THRESH_OTSU);
 
 		findContours(bin, contours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
 		for (vector<Point> contour : contours) {
+			// threshold
+			// 검출한 contours를 최소 영역의 직사각형으로 감쌌을 때의 영역이 100 이하라면 노이즈로 보고 무시
 			if (contourArea(contour) < 100) {
 				continue;
 			}
@@ -72,8 +77,10 @@ int main(void)
 
 			Mat roi = frame(Rect(Point(rect.x, rect.y),
 				Point(rect.x + rect.width, rect.y + rect.height)));
-			imshow("ROI", roi);
+			// imshow("roi", roi);
 
+			// 16 프레임마다 검출된 ROI를 저장
+			// 테스트 시 경로 수정
 			if ((count % 16) == 0) {
 				imwrite(format("C:/Users/hisn16.DESKTOP-HGVGADP/source/repos/OpenCV_Project/OpenCV_Project/ROI/%dth_ROI.png", count), roi);
 			}
